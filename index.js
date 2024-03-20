32bc051f1b1b65819dfda156fcfc75c5bf2fca68
noyxmon <156950291+noyxmon@users.noreply.github.com>
2024-03-20 11:05:10 +0530

    Update index.js Added Dnd Mode

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ manager.spawn();
 client.on('ready', async () => {
   console.log(`${client.user.tag} is online!`);
   client.user.setActivity('Xcia'); // Replace with the game name
-client.user.setStatus('dnd');
+  client.user.setStatus('dnd');
 });
 
 } else {
