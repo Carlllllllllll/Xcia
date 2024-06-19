6fec631d09ecb8a2dd448be66ac5160c2e64cbec
Lazy Xmo <156950291+noyxmon@users.noreply.github.com>
2024-06-19 22:19:55 +0530

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -15,8 +15,10 @@ async function play(client, interaction) {
             deaf: true
         });
 
+     
         await interaction.deferReply();
 
+   
         const resolve = await client.riffy.resolve({ query: query, requester: interaction.user });
         const { loadType, tracks, playlistInfo } = resolve;
 
@@ -43,43 +45,55 @@ async function play(client, interaction) {
                 .setTitle('Error')
                 .setDescription('There are no results found.');
 
+        
             await interaction.editReply({ embeds: [errorEmbed] });
             return;
         }
 
-        const embeds = [
-            new EmbedBuilder()
-                .setColor('#4d9fd6')
-                .setAuthor({
-                    name: 'Added To Queue/Playing',
-                    iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1236794583732457473/7828-verify-ak.gif?ex=66394e37&is=6637fcb7&hm=923d3f3b300606a2ae4ceb7bae980fd533a4c5ee2cf73111569a892a595f1f69&', 
-                    url: 'https://discord.gg/P47geahQ4Z'
-                })
-                .setDescription('<a:arrow:1215601724341878815> **Moj Maro With No Limitations**'),
+       
+        await new Promise(resolve => setTimeout(resolve, 500));
+        
+     
+const { EmbedBuilder } = require("discord.js");
 
-            new EmbedBuilder()
-                .setColor('#ffea00')
-                .setAuthor({
-                    name: 'Added To Queue/Playing',
-                    iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1236802032938127470/4104-verify-yellow.gif?ex=66395527&is=663803a7&hm=71a7fba7f91897e52d9645b45d85d3da0ff97af2b63d10960004e68ff40d9c3b&', 
-                    url: 'https://discord.gg/P47geahQ4Z'
-                })
-                .setDescription('<a:arrow:1215601724341878815> **Moj Maro With No Limitations**'),
 
-            new EmbedBuilder()
-                .setColor('#FF0000')
-                .setAuthor({
-                    name: 'Added To Queue/Playing',
-                    iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1236802049190920202/4104-verify-red.gif?ex=6639552b&is=663803ab&hm=8dbc851fe56441b916a0044152dd517ed26434a11ee506518382f380c527c3bd&', 
-                    url: 'https://discord.gg/P47geahQ4Z'
-                })
-                .setDescription('<a:arrow:1215601724341878815> **Moj Maro With No Limitations**')
-        ];
+const embeds = [
+  
+    new EmbedBuilder()
+        .setColor('#4d9fd6')
+        .setAuthor({
+            name: 'Added To Queue/Playing',
+            iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1236794583732457473/7828-verify-ak.gif?ex=66394e37&is=6637fcb7&hm=923d3f3b300606a2ae4ceb7bae980fd533a4c5ee2cf73111569a892a595f1f69&', 
+            url: 'https://discord.gg/P47geahQ4Z'
+        })
+        .setDescription('<a:arrow:1215601724341878815> **Moj Maro With No Limitations**'),
 
-        const randomIndex = Math.floor(Math.random() * embeds.length);
+ 
+    new EmbedBuilder()
+    .setColor('#ffea00')
+    .setAuthor({
+        name: 'Added To Queue/Playing',
+        iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1236802032938127470/4104-verify-yellow.gif?ex=66395527&is=663803a7&hm=71a7fba7f91897e52d9645b45d85d3da0ff97af2b63d10960004e68ff40d9c3b&', 
+        url: 'https://discord.gg/P47geahQ4Z'
+    })
+    .setDescription('<a:arrow:1215601724341878815> **Moj Maro With No Limitations**'),
+    
+  
+    new EmbedBuilder()
+    .setColor('#FF0000')
+    .setAuthor({
+        name: 'Added To Queue/Playing',
+        iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1236802049190920202/4104-verify-red.gif?ex=6639552b&is=663803ab&hm=8dbc851fe56441b916a0044152dd517ed26434a11ee506518382f380c527c3bd&', 
+        url: 'https://discord.gg/P47geahQ4Z'
+    })
+    .setDescription('<a:arrow:1215601724341878815> **Moj Maro With No Limitations**')
+];
 
-        await channel.send(`${interaction.user.username} Played Smth In ${interaction.guild.name}`);
-        await interaction.reply({ embeds: [embeds[randomIndex]] });
+
+const randomIndex = Math.floor(Math.random() * embeds.length);
+
+await channel.send(`${interaction.user.username} Played Smth In ${interaction.guild.name}`);
+await interaction.reply({ embeds: [embeds[randomIndex]] });
 
     } catch (error) {
         console.error('Error processing play command:', error);
@@ -87,8 +101,6 @@ async function play(client, interaction) {
             .setColor('#ff0000')
             .setTitle('Error')
             .setDescription('An error occurred while processing your request.');
-
-        await interaction.reply({ embeds: [errorEmbed] });
     }
 }
 
