126a517e6200827390d569b2f140b32b4376ba93
Lazy Xmo <156950291+noyxmon@users.noreply.github.com>
2024-06-19 23:04:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  TOKEN: "MTE5NzYwNTg2MjE1NjU1NDQ1NA.GsgSSZ.5eWLruZ5g1xgFagRuyMHtgL3D-au-OnjEvL04k",
-  ownerID: ["886301232266870794"], 
+  TOKEN: "MTE5NzYwNTg2MjE1NjU1NDQGsgSSZ.5eWLruZ5g1xgFagRuyMHtgL3D-au-OnjEvL04k",
+  ownerID: ["886301232266794"], 
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "00fbff",
